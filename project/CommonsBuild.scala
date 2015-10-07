5b7fb388d40e2013bc0fd1439676a00080b2a6af
prettynatty <fear@loathing.in>
2015-10-07 19:04:09 +0300

    added pomExtra

--- project/CommonsBuild.scala
+++ project/CommonsBuild.scala
@@ -7,6 +7,27 @@ object CommonsBuild {
     scalaVersion := "2.11.7",
     resolvers ++= Seq(
       Resolver.sonatypeRepo("releases")
-    )
+    ),
+    pomExtra := (
+      <url>http://github.com/actorapp/actor-commons</url>
+        <licenses>
+          <license>
+            <name>MIT</name>
+            <url>http://www.opensource.org/licenses/MIT</url>
+            <distribution>repo</distribution>
+          </license>
+        </licenses>
+        <scm>
+          <url>git@github.com:actorapp/commons.git</url>
+          <connection>scm:git:git@github.com:actorapp/commons.git</connection>
+        </scm>
+        <developers>
+          <developer>
+            <id>prettynatty</id>
+            <name>Andrey Kuznetsov</name>
+            <url>http://fear.loathing.in</url>
+          </developer>
+        </developers>
+      )
   )
 }
