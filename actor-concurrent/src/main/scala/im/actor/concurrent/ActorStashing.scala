ca4a8358a3b4a68eebf93317f2f9a9227f150e9f
prettynatty <fear@loathing.in>
2016-01-10 23:34:49 +0300

    feat: becomeStashing accepts discardOld

--- actor-concurrent/src/main/scala/im/actor/concurrent/ActorStashing.scala
+++ actor-concurrent/src/main/scala/im/actor/concurrent/ActorStashing.scala
@@ -2,11 +2,9 @@ package im.actor.concurrent
 
 import akka.actor.{ Stash, ActorRef, Actor }
 
-trait ActorStashing {
-  self: Actor with Stash ⇒
-
-  protected def becomeStashing(f: ActorRef ⇒ Receive): Unit =
-    context.become(f(sender()) orElse stashing, discardOld = false)
+trait ActorStashing extends Actor with Stash{
+  protected def becomeStashing(f: ActorRef ⇒ Receive, discardOld: Boolean = false): Unit =
+    context.become(receiveStashing(f), discardOld = discardOld)
 
   protected def receiveStashing(f: ActorRef ⇒ Receive): Receive =
     f(sender()) orElse stashing
